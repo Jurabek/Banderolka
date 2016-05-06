80318de3d68ec75b3787fcb9a469a8340420f2cd
Jurabek <Jurabek@ASUS-G551JM>
2016-05-06 19:19:54 +0500

    Add project files.

---
+++ Banderolka/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Banderolka.MvcApplication" Language="C#" %>
---
+++ Banderolka/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Banderolka.Startup))]
+namespace Banderolka
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
